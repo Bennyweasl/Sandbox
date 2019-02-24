fba1fc24c17439a7244ada43b440e799e40cc2ff
benjamin baysinger <ben@highrscout.com>
2019-02-24 08:10:02 -0800

    source folder made

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,8 +1,12 @@
+import java.util.List;
+
+import javafx.stage.Window;
+
 public class HelloWorld {
 
     public static void main(String[] args) {
         // Prints "Hello, World" to the terminal window.
         System.out.println("Hello World");
     }
-
+    List myFirstListInVsCode = new ArrayList();
 }
